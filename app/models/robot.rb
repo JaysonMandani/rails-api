c5bdaf6ab23b3ddf6be64b027f77be0952761bbd
Jayson Mandani <jaysonmandani@gmail.com>
2018-03-09 23:16:54 +0800

    Adds scopes on robot

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,2 +1,7 @@
 class Robot < ApplicationRecord
+  scope :has_rotors, -> { where("number_of_rotors <= ? AND number_of_rotors >= ?", 3, 8) }
+  scope :has_colour, -> { where(colour: 'blue') }
+  scope :has_track_wheels, -> { where.not(has_wheels: true, has_tracks: true) }
+  scope :has_sentience_status, -> { where(has_sentience: true, statuses: 'loose screws') }
+  scope :has_onfire, -> { where(statuses: 'on fire') }
 end
