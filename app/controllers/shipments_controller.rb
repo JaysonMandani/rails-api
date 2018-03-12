376f64903a8ab97d413c5e7285861166e9172736
Jayson Mandani <jaysonmandani@gmail.com>
2018-03-12 20:40:28 +0800

    Adds api and method on shipment

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,4 +1,17 @@
 class ShipmentsController < ApplicationController
   def create
+    @shipment = Shipment.create(shipment_params)
+    if @shipment.present?
+      @shipment.update_shipped(shipment_params.robot_ids)
+      render json: "Successfully created", status: :created
+    else
+      render json: @shipment.errors.full_messages, status: :failed
+    end
+  end
+
+  private
+
+  def shipment_params
+    params.require(:shipment).permit(:robot_ids)
   end
 end
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,2 +1,6 @@
 class Shipment < ApplicationRecord
+  def update_shipped robot_ids
+    @robots = Robot.find(robot_ids)
+    @robots.map{ |x| x.update(shipped: true) }
+  end
 end
